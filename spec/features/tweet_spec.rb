c8304fa34bd8da24f27893e488fe7f30b7653045
saito yuko <y_s0617@yahoo.co.jp>
2019-11-17 11:28:08 +0900

    ログイン処理の再現（テスト）

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -10,4 +10,12 @@ feature 'tweet', type: :feature do
     visit root_path
     expect(page).to have_no_content('投稿する')
   end
+
+  visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('投稿する')
+  end
 end
